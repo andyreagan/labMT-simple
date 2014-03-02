38e12ca06288ed87fa2cc8b42e807e1c31128e2c
Andy Reagan <github@andyreagan.com>
2014-03-01 20:54:55 -0500

    added a simple test of the shift

---
+++ test.py
@@ -0,0 +1,57 @@
+## set up
+from storyLab import *
+labMT,labMTvector,labMTwordList = emotionFileReader(returnVector=True)
+
+## for testing
+happyWords = "happy happy happy happy"
+sadWords = "sad sad sad sad sad"
+
+## for more testing
+f = open("lance.txt","r")
+happyWords = f.read()
+f.close()
+f = open("gop.txt","r")
+sadWords = f.read()
+f.close()
+
+## test vectors
+happyHapps,happyHappsL = emotion(happyWords,labMT,shift=True,happsList=labMTvector)
+sadHapps,sadHappsL = emotion(sadWords,labMT,shift=True,happsList=labMTvector)
+print happyHapps
+print sadHapps
+## print max(sadHappsL)
+
+## make a shift: shift(values,ref,comp)
+shiftMag,shiftType = shift(labMTvector,happyHappsL,sadHappsL)
+shiftMagAbs = map(abs,shiftMag)
+
+## sort them both
+indices = sorted(range(len(shiftMag)), key=shiftMagAbs.__getitem__, reverse=True)
+sortedMag = [shiftMag[i] for i in indices]
+sortedType = [shiftType[i] for i in indices]
+sortedWords = [labMTwordList[i] for i in indices]
+  
+print indices[0:10]
+print sortedMag[0:20]
+print sortedType[0:20]
+print sortedWords[0:20]
+
+f = open("sampleSortedMag.csv","w")
+for val in sortedMag:
+  f.write(str(val))
+  f.write("\n")
+f.close()
+
+f = open("sampleSortedType.csv","w")
+for val in sortedType:
+  f.write(str(val))
+  f.write("\n")
+f.close()
+
+f = open("sampleSortedWords.csv","w")
+for val in sortedWords:
+  f.write(val)
+  f.write("\n")
+f.close()
+
+
