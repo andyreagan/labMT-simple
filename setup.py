4a9c0d350daef6eb675c9030b0526fe461e332f5
Andy Reagan <github@andyreagan.com>
2014-03-13 12:41:51 -0400

    set up for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+    setup(
+        name = 'labMTsimple',
+        packages = ['labMTsimple'],
+        version = '0.1',
+        description = 'Basic usage script for LabMT1.0 dataset',
+        author = 'Andy Reagan',
+        author_email = 'andy@andyreagan.com',
+        url = 'https://github.com/andyreagan/labMT-simple', 
+        download_url = 'https://github.com/andyreagan/labMT-simple/tarball/0.1',
+        keywords = [],
+        classifiers = [],
+    )
---
+++ test/test.py
@@ -0,0 +1,65 @@
+## set up
+from storyLab import *
+labMT,labMTvector,labMTwordList = emotionFileReader(returnVector=True)
+
+## take a look at these guys
+print labMT['laughter']
+print labMTvector[0:5]
+print labMTwordList[0:5]
+
+## test shift a subsample of two twitter days
+import codecs ## handle utf8
+f = codecs.open("25.01.14.txt","r","utf8")
+saturday = f.read()
+f.close()
+f = codecs.open("28.01.14.txt","r","utf8")
+tuesday = f.read()
+f.close()
+
+## compute valence score
+saturdayValence = emotion(saturday,labMT)
+tuesdayValence = emotion(tuesday,labMT)
+print 'the valence of {0} is {1}'.format('saturday',saturdayValence)
+print 'the valence of {0} is {1}'.format('tuesday',tuesdayValence)
+
+## compute valence score and return frequency vector for generating wordshift
+saturdayValence,saturdayFvec = emotion(saturday,labMT,shift=True,happsList=labMTvector)
+tuesdayValence,tuesdayFvec = emotion(tuesday,labMT,shift=True,happsList=labMTvector)
+
+## make a shift: shift(values,ref,comp)
+shiftMag,shiftType = shift(labMTvector,tuesdayFvec,saturdayFvec)
+## take the absolute value of the shift magnitude
+shiftMagAbs = map(abs,shiftMag)
+
+## sort them both
+indices = sorted(range(len(shiftMag)), key=shiftMagAbs.__getitem__, reverse=True)
+sortedMag = [shiftMag[i] for i in indices]
+sortedType = [shiftType[i] for i in indices]
+sortedWords = [labMTwordList[i] for i in indices]
+
+## take a peek at the top words  
+print indices[0:10]
+print sortedMag[0:20]
+print sortedType[0:20]
+print sortedWords[0:20]
+
+## print each of these to a file
+f = open("sampleSortedMag.csv","w")
+for val in sortedMag:
+  f.write(str(val))
+  f.write("\n")
+f.close()
+
+f = open("sampleSortedType.csv","w")
+for val in sortedType:
+  f.write(str(val))
+  f.write("\n")
+f.close()
+
+f = open("sampleSortedWords.csv","w")
+for val in sortedWords:
+  f.write(val)
+  f.write("\n")
+f.close()
+
+
