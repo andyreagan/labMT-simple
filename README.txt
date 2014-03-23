bf5a9fc34776d48346cd61fab8d5e7293234ef27
Andy Reagan <github@andyreagan.com>
2014-03-23 02:01:26 -0400

    map is working...saving

--- README.txt
+++ README.txt
@@ -25,79 +25,40 @@ the test directory, using the command
 
     python test.py
 
-and opening the file shiftPlot.html. For an explanation of the resulting
+and opening the file test.html. For an explanation of the resulting
 plot, please visit
 
 http://www.hedonometer.org/shifts.html
 
-Usage
------
+Installation
+------------
 
-The Python script test.py uses this module to test a subsample of
-Twitter data:
+Cloning the github directly is recommended, i.e.
 
-.. code:: python
+.. code:: bash
 
-    from storyLab import *
-    labMT,labMTvector,labMTwordList = emotionFileReader(returnVector=True)
+    git clone https://github.com/andyreagan/labMT-simple.git
 
-    ## take a look at these guys
-    print labMT['laughter']
-    print labMTvector[0:5]
-    print labMTwordList[0:5]
+and then installing locally using
 
-    ## test shift a subsample of two twitter days
-    import codecs ## handle utf8
-    f = codecs.open("25.01.14.txt","r","utf8")
-    saturday = f.read()
-    f.close()
-    f = codecs.open("28.01.14.txt","r","utf8")
-    tuesday = f.read()
-    f.close()
+.. code:: bash
 
-    ## compute valence score
-    saturdayValence = emotion(saturday,labMT)
-    tuesdayValence = emotion(tuesday,labMT)
-    print 'the valence of {0} is {1}'.format('saturday',saturdayValence)
-    print 'the valence of {0} is {1}'.format('tuesday',tuesdayValence)
+    sudo python setup.py install
 
-    ## compute valence score and return frequency vector for generating wordshift
-    saturdayValence,saturdayFvec = emotion(saturday,labMT,shift=True,happsList=labMTvector)
-    tuesdayValence,tuesdayFvec = emotion(tuesday,labMT,shift=True,happsList=labMTvector)
+Tests can be run by navigating to the test directory, and running
 
-    ## make a shift: shift(values,ref,comp)
-    shiftMag,shiftType = shift(labMTvector,tuesdayFvec,saturdayFvec)
-    ## take the absolute value of the shift magnitude
-    shiftMagAbs = map(abs,shiftMag)
+.. code:: bash
 
-    ## sort them both
-    indices = sorted(range(len(shiftMag)), key=shiftMagAbs.__getitem__, reverse=True)
-    sortedMag = [shiftMag[i] for i in indices]
-    sortedType = [shiftType[i] for i in indices]
-    sortedWords = [labMTwordList[i] for i in indices]
+    python test.py
 
-    ## take a peek at the top words  
-    print indices[0:10]
-    print sortedMag[0:20]
-    print sortedType[0:20]
-    print sortedWords[0:20]
+which will compare the two days in test/data and print test.html which
+shifts them, allowing for a changable lens.
 
-    ## print each of these to a file
-    f = open("sampleSortedMag.csv","w")
-    for val in sortedMag:
-      f.write(str(val))
-      f.write("\n")
-    f.close()
+This repository can also be installed using pip
 
-    f = open("sampleSortedType.csv","w")
-    for val in sortedType:
-      f.write(str(val))
-      f.write("\n")
-    f.close()
+.. code:: bash
 
-    f = open("sampleSortedWords.csv","w")
-    for val in sortedWords:
-      f.write(val)
-      f.write("\n")
-    f.close()
+    pip install labMTsimple
 
+in which case you can download the tests from github and run them, if
+desired.
---
+++ test/geoMain.js
@@ -0,0 +1,101 @@
+function initializePlot() {
+    loadCsv();
+}
+
+allStateNames = ["Alabama","Alaska","Arizona","Arkansas","California","Colorado","Connecticut"
+,"Delaware","Florida","Georgia","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana"
+,"Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska"
+,"Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Ohio"
+,"Oklahoma","Oregon","Pennsylvania","Rhode Island","South Carolina","South Dakota","Tennessee"
+,"Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin","Wyoming","District of Columbia"]
+
+function loadCsv() {
+var csvLoadsRemaining = 4;
+d3.text("labMTvecGeo.csv", function(text) {
+    var tmp = text.split("\n");
+    lens = tmp.map(parseFloat);
+    if (!--csvLoadsRemaining) initializePlotPlot(lens,words);
+});
+d3.text("labMTwordsGeo.csv", function(text) {
+    var tmp = text.split("\n");
+    words = tmp;
+    if (!--csvLoadsRemaining) initializePlotPlot(lens,words);
+});
+d3.json("static/us-states.topojson", function(data) {
+    geoJson = data;
+    console.log(geoJson);
+    if (!--csvLoadsRemaining) initializePlotPlot(lens,words);
+});
+d3.text("data/wordCounts2013.csv", function(text) {
+    tmp = text.split("\n");
+    allData = Array(51);
+    for (var i=0; i<allData.length; i++) {
+	allData[i] = {name: allStateNames[i],
+		      rawFreq: tmp[i].split(","),
+		      freq: tmp[i].split(",")};
+    }
+    stateFeatures = topojson.feature(geoJson,geoJson.objects.states).features;
+
+    //Merge the ag. data and GeoJSON
+    //Loop through once for each ag. data value
+    for (var i = 0; i < allData.length; i++) {
+	
+	//Grab state name
+	var stateName = allData[i].name;
+	
+	//Grab data value, and convert from string to float
+	var stateVal = allData[i].avhapps;
+	
+	//Find the corresponding state inside the GeoJSON
+	for (var j = 0; j < stateFeatures.length; j++) {
+
+	    var jsonState = stateFeatures[j].properties.name;
+	    
+	    if (stateName == jsonState) {
+		
+		//Copy the data value into the JSON
+		stateFeatures[j].properties.avhapps = stateVal;
+		
+		//Stop looking through the JSON
+		break;
+	    }
+	}		
+    }
+    if (!--csvLoadsRemaining) initializePlotPlot(lens,words);
+});
+};
+
+function initializePlotPlot(lens,words) {
+    // draw the lens
+    drawLensGeo(d3.select("#lens01"),lens);
+
+    // initially apply the lens, and draw the shift
+    for (var j=0; j<allData.length; j++) {
+	for (var i=0; i<allData[j].rawFreq.length; i++) {
+	    if (lens[i] > 4 && lens[i] < 6) {
+		allData[j].freq[i] = 0;
+            }
+	    else {
+		allData[j].freq[i] = allData[j].rawFreq[i];
+	    }
+	}
+    }
+
+    computeHapps();
+
+    shiftObj = shift(allData[0].freq,allData[1].freq,lens,words);
+    plotShift(d3.select("#shift01"),shiftObj.sortedMag.slice(0,200),
+              shiftObj.sortedType.slice(0,200),
+              shiftObj.sortedWords.slice(0,200),
+              shiftObj.sumTypes,
+              shiftObj.refH,
+              shiftObj.compH);
+
+    drawMap(d3.select('#map01'))
+
+};
+
+initializePlot();
+
+
+
