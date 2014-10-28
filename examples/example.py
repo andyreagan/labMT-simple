45ec878e2d180c865be6e7efb17b4cb138b8dfdf
Andy Reagan <github@andyreagan.com>
2014-10-27 21:06:39 -0400

    fixing the different labMT files

--- examples/example.py
+++ examples/example.py
@@ -3,7 +3,8 @@ from labMTsimple.storyLab import *
 import codecs ## handle utf8
 
 if __name__ == '__main__':
-  labMT,labMTvector,labMTwordList = emotionFileReader(stopval = 0.0, returnVector=True)
+  lang = 'english'
+  labMT,labMTvector,labMTwordList = emotionFileReader(stopval = 0.0,fileName='labMT2'+lang,returnVector=True)
   
   ## take a look at these guys
   print 'the word laughter in the hash has the data:'
@@ -28,37 +29,45 @@ if __name__ == '__main__':
   tuesdayValence,tuesdayFvec = emotion(tuesday,labMT,shift=True,happsList=labMTvector)
   print 'the valence of {0} is {1:.5}'.format('saturday',saturdayValence)
   print 'the valence of {0} is {1:.5}'.format('tuesday',tuesdayValence)
+
+  print tuesdayFvec[:10]
+  print tuesdayFvec[9522]
+
+  writeCsv = False
+  if writeCsv:
+    f = open("saturdayFvec.csv","w")
+    f.write('{0:.0f}'.format(saturdayFvec[0]))
+    for i in xrange(1,len(saturdayFvec)):
+      f.write("\n")
+      f.write('{0:.0f}'.format(saturdayFvec[i]))
+    f.close()
   
-  f = open("saturdayFvec.csv","w")
-  f.write('{0:.0f}'.format(saturdayFvec[0]))
-  for i in xrange(1,len(saturdayFvec)):
-    f.write("\n")
-    f.write('{0:.0f}'.format(saturdayFvec[i]))
-  f.close()
+    f = open("tuesdayFvec.csv","w")
+    f.write('{0:.0f}'.format(tuesdayFvec[0]))
+    for i in xrange(1,len(tuesdayFvec)):
+      f.write("\n")
+      f.write('{0:.0f}'.format(tuesdayFvec[i]))
+    f.close()
   
-  f = open("tuesdayFvec.csv","w")
-  f.write('{0:.0f}'.format(tuesdayFvec[0]))
-  for i in xrange(1,len(tuesdayFvec)):
-    f.write("\n")
-    f.write('{0:.0f}'.format(tuesdayFvec[i]))
-  f.close()
+    f = open("labMTvec.csv","w")
+    f.write('{0:.8f}'.format(labMTvector[0]))
+    for i in xrange(1,len(labMTvector)):
+      f.write("\n")
+      f.write('{0:.8f}'.format(labMTvector[i]))
+    f.close()
   
-  f = open("labMTvec.csv","w")
-  f.write('{0:.8f}'.format(labMTvector[0]))
-  for i in xrange(1,len(labMTvector)):
-    f.write("\n")
-    f.write('{0:.8f}'.format(labMTvector[i]))
-  f.close()
-  
-  f = open("labMTwords.csv","w")
-  f.write(labMTwordList[0])
-  for i in xrange(1,len(labMTwordList)):
-    f.write("\n")
-    f.write(labMTwordList[i])
-  f.close()
+    f = open("labMTwords.csv","w")
+    f.write(labMTwordList[0])
+    for i in xrange(1,len(labMTwordList)):
+      f.write("\n")
+      f.write(labMTwordList[i])
+    f.close()
 
   tuesdayStoppedVec = stopper(tuesdayFvec,labMTvector,labMTwordList,stopVal=1.0)
   saturdayStoppedVec = stopper(saturdayFvec,labMTvector,labMTwordList,stopVal=1.0)
+
+  print tuesdayStoppedVec[:10]
+  print tuesdayStoppedVec[9522]
   
   shiftHtml(labMTvector,labMTwordList,tuesdayStoppedVec,saturdayStoppedVec,sys.argv[1])
 
